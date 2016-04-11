c62031a3694c34ed020efd545e6ccd6dcd405cf0
Katarzyna Niewczas <kasia-niewczas@wp.pl>
2016-04-11 12:12:39 +0200

    updates

---
+++ laboratoria/lab02/messenger/src/test/java/com/example/mockdemo/app/MessengerTest.java
@@ -0,0 +1,34 @@
+package com.example.mockdemo.app;
+import static org.easymock.EasyMock.*;
+import static org.junit.Assert.assertEquals;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.example.mockdemo.messenger.ConnectionStatus;
+import com.example.mockdemo.messenger.MessageService;
+
+
+public class MessengerTest {
+
+	private Messenger messenger;
+	private MessageService mock;
+
+	static final String VALID_SERVER = "inf.ug.edu.pl";
+	static final String VALID_MESSAGE = "some message";
+	
+	@Before
+	public void setUp() {
+		mock = createMock(MessageService.class);
+		messenger = new Messenger(mock);
+	}
+
+	@Test
+	public void testConnectionWithResult0() {
+		expect(mock.checkConnection(VALID_SERVER)).andReturn(ConnectionStatus.SUCCESS).atLeastOnce();
+		replay(mock);
+		assertEquals(0, messenger.testConnection(VALID_SERVER));
+		verify(mock);
+	}
+}
